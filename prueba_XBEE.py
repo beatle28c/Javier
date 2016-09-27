795fe2cf127bb4435bee1ce9aa4295228216ef89
beatle28c <nautilus28c@gmail.com>
2016-09-26 22:10:42 -0600

    prueba_XBEE.py

--- prueba_XBEE.py
+++ prueba_XBEE.py
@@ -3,24 +3,28 @@ import cv2
 import sys
 import RPi.GPIO as GPIO
 import time
+import serial
 
-a1=0
-b1=mh
-a2=2*mw
-b2=mh
-cuenta=0
-minLineLength = 100
-maxLineGap = 10
 
 ##PWM y IO de la Raspberry##
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT,initial=GPIO.low)
+GPIO.setup(14, GPIO.OUT,initial=GPIO.LOW)
 GPIO.setup(15, GPIO.IN)
 
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+string = 'Hello from Raspberry Pi'
+print 'Sending "%s"' % string
+ser.write('%s\n' % string)
+
 while True:
-	
-	cond=GPIO.input(15);
-	if cond==TRUE:
-		led = GPIO.output(14, GPIO.HIGH)
-	else:
-		led = GPIO.output(14, GPIO.LOW)
+	incoming = ser.readline().strip()
+	print 'Received %s' % incoming
+	ser.write('RPi Received: %s\n' % incoming)
+
+#while True:	
+#	cond=GPIO.input(15);
+#	if cond==True:
+#		led = GPIO.output(14, GPIO.HIGH)
+#	else:
+#		led = GPIO.output(14, GPIO.LOW)
